d30edff4601cd208acfeae679023edbf1a8f8abb
vajramsrujan <55774184+vajramsrujan@users.noreply.github.com>
2021-04-13 23:26:01 -0400

    Added UI elements and basic navigation
    - Installed react-navigation-elements
    
    - Added UI elements to:
    SignInScreen
    CreateRoomScreen
    JoinRoomScreen
    MenuScreen
    
    - Added basic navigation between aformentioned screens.
    
    - Added Spacing component

--- SoundOff/src/screens/CreateRoomScreen.js
+++ SoundOff/src/screens/CreateRoomScreen.js
@@ -1,18 +1,47 @@
-import React from 'react'; 
+import React, {useEffect, useState} from 'react'; 
 import {Text, View, StyleSheet} from 'react-native'
+import {Button} from 'react-native-elements'
+import Spacer from '../components/Spacer'
 
 const CreateRoomScreen = ({navigation}) => { 
     
+    // State keeps track of randomly generated key
+    const [randomKey, setRandomKey] = useState(''); 
+
+    // useEffect to make sure random key is only rendered once upon screen access
+    useEffect(() => {
+        let key = Math.random().toString().substr(2, 6); // Random number key
+        setRandomKey(key);
+    }, []); 
+
     return (
         <View>
-            <Text>CreateRoomScreen</Text>
+            <Text style={styles.header} h2>Room created!</Text> 
+            <Text style={styles.roomKey}> Room key is: {randomKey}</Text>
+            <Spacer/>
+            <Button 
+                    title="Begin game"
+                    onPress={() => navigation.navigate('Menu')}
+            />
         </View>
-    )
-
-    
+    )  
 }
-
 const styles = StyleSheet.create({
+
+    header: {
+        marginTop: 100,
+        fontSize: 30,
+        justifyContent: "center",
+        textAlign: 'center'
+    },
+
+    roomKey: {
+        marginTop: 50,
+        fontSize: 20,
+        justifyContent: "center",
+        textAlign: 'center'
+    }
+    
 })
 
 export default CreateRoomScreen;
--- SoundOff/src/screens/MenuScreen.js
+++ SoundOff/src/screens/MenuScreen.js
@@ -1,18 +1,40 @@
 import React from 'react'; 
 import {Text, View, StyleSheet} from 'react-native'
+import {Button} from 'react-native-elements'
+import Spacer from '../components/Spacer' 
 
 const MenuScreen = ({navigation}) => { 
     
     return (
         <View>
-            <Text>MenuScreen</Text>
+            <Text style={styles.header} h2>Main Menu</Text> 
+            
+            <View>
+                <Spacer/>
+                <Button 
+                    title="Create a Room!"
+                    containerStyle={{ marginVertical: 20 }}
+                    onPress={() => navigation.navigate('CreateRoom')}
+                />
+                <Button 
+                    title="Join a Room!"
+                    onPress={() => navigation.navigate('JoinRoom')}
+                />
+                <Spacer/>
+            </View>
         </View>
     )
-
-    
 }
 
 const styles = StyleSheet.create({
+
+    header: {
+        marginTop: 100,
+        fontSize: 30,
+        justifyContent: "center",
+        textAlign: 'center'
+    }
+
 })
 
 export default MenuScreen;
