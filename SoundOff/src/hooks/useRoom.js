0970928291534979a7d8fd01202eecaf72c3e5c0
Kyle Martin <ksmartin@bu.edu>
2021-05-23 22:10:06 -0400

    Add sign in and create room functionality

--- SoundOff/src/hooks/useRoom.js
+++ SoundOff/src/hooks/useRoom.js
@@ -1,16 +1,20 @@
-import {useState } from 'react';
+import {useState, useContext} from 'react';
 import {roomGet, roomPost} from '../api/gamestate';
+import {Context as AuthContext} from '../context/AuthContext';
 
 export default () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
+  const {
+    state: {userToken}
+  } = useContext(AuthContext);
   
   const roomApi = async (query, roomID, randomKey) => {
 
     if (query === '/get') {
         
         try {
-            const response = await roomGet(roomID, randomKey)
+            const response = await roomGet(roomID, randomKey, userToken)
             setResults(response);
           } catch (err) {
             setErrorMessage('Something went wrong');
@@ -21,7 +25,7 @@ export default () => {
     else if (query === '/post') {
 
         try {
-            const response = await roomPost(roomID, randomKey)
+            const response = await roomPost(roomID, randomKey, userToken)
             setResults(response);
           } catch (err) {
             setErrorMessage('Something went wrong');
