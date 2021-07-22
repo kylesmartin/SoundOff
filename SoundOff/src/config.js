1655f17de19ac726900664f8be30c5abda6b0821
Kyle Martin <ksmartin@bu.edu>
2021-07-22 13:54:32 -0400

    Add transition from join/create room to song search

--- SoundOff/src/config.js
+++ SoundOff/src/config.js
@@ -1,2 +1,2 @@
-export const SOCKET_URL = 'http://a5da6c81ec1a.ngrok.io'; // 8080
-export const SERVER_URL = 'http://c01f5bb1c275.ngrok.io'; // 8888
+export const SOCKET_URL = 'http://813f8da5399c.ngrok.io'; // 28200
+export const SERVER_URL = 'http://6621df532f59.ngrok.io'; // 30000
